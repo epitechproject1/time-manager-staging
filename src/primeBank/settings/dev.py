8d11f8a1e8cf3c79487cdc132b2e8e193ef3f69b
Eis-Pirat <djalilelyassaa@gmail.com>
2026-02-05 17:18:29 +0100

    fix(ci): remove subprocess and initialize django correctly

--- src/primeBank/settings/dev.py
+++ src/primeBank/settings/dev.py
@@ -1,47 +1,7 @@
-from . import base
-
-# =============================================================================
-# ENV DEV
-# =============================================================================
+from .base import *  # noqa
 
 DEBUG = True
-
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
-# =============================================================================
-# TEMPLATES
-# =============================================================================
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [base.BASE_DIR / "templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
-# =============================================================================
-# DATABASE (SQLite DEV)
-# =============================================================================
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": base.BASE_DIR / "db.sqlite3",
-    }
-}
-
-# =============================================================================
-# DJANGO REST FRAMEWORK
-# =============================================================================
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
@@ -52,21 +12,3 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
-
-# =============================================================================
-# REQUIRED DJANGO SETTINGS (re-export)
-# =============================================================================
-
-BASE_DIR = base.BASE_DIR
-SECRET_KEY = base.SECRET_KEY
-INSTALLED_APPS = base.INSTALLED_APPS
-MIDDLEWARE = base.MIDDLEWARE
-ROOT_URLCONF = base.ROOT_URLCONF
-WSGI_APPLICATION = base.WSGI_APPLICATION
-LANGUAGE_CODE = base.LANGUAGE_CODE
-TIME_ZONE = base.TIME_ZONE
-USE_I18N = base.USE_I18N
-USE_TZ = base.USE_TZ
-STATIC_URL = base.STATIC_URL
-STATIC_ROOT = base.STATIC_ROOT
-DEFAULT_AUTO_FIELD = base.DEFAULT_AUTO_FIELD
