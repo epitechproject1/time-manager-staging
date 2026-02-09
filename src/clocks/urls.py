312b128e43bce1b537c04f0fb97648b85b746692
Princy Samba <princyrovnelsamba@gmail.com>
2026-02-09 12:51:24 +0100

    feature_departements: add clocks extend shema

--- src/clocks/urls.py
+++ src/clocks/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import ClockViewSet
 
+
 router = DefaultRouter()
 router.register("clocks", ClockViewSet, basename="clocks")
 
--- src/clocks/views.py
+++ src/clocks/views.py
@@ -1,9 +1,34 @@
-from rest_framework import permissions, viewsets
-
+from rest_framework import viewsets, permissions
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from .models import Clock
 from .serializers import ClockSerializer
 
-
+@extend_schema_view(
+    list=extend_schema(
+        tags=["Clocks"],
+        summary="Lister tous les pointages",
+    ),
+    retrieve=extend_schema(
+        tags=["Clocks"],
+        summary="Détail d’un pointage",
+    ),
+    create=extend_schema(
+        tags=["Clocks"],
+        summary="Créer un nouveau pointage",
+    ),
+    update=extend_schema(
+        tags=["Clocks"],
+        summary="Mettre à jour un pointage",
+    ),
+    partial_update=extend_schema(
+        tags=["Clocks"],
+        summary="Mettre à jour partiellement un pointage",
+    ),
+    destroy=extend_schema(
+        tags=["Clocks"],
+        summary="Supprimer un pointage",
+    ),
+)
 class ClockViewSet(viewsets.ModelViewSet):
     queryset = Clock.objects.all()
     serializer_class = ClockSerializer
