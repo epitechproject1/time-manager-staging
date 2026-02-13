7c767cdcf2d1227014259619a6144d0462869095
Eis-Pirat <161588358+Eis-Pirat@users.noreply.github.com>
2026-02-13 12:45:29 +0100

    Refactor permission create serializer test
    Refactor test_permission_create_serializer_valid to use timezone.now() directly for start_date.

--- src/permissions/tests/test_serializers.py
+++ src/permissions/tests/test_serializers.py
@@ -27,17 +27,10 @@ def test_permission_serializer(permission):
 
 @pytest.mark.django_db
 def test_permission_create_serializer_valid(admin_user, normal_user):
-    future_date = timezone.now().date() + timedelta(days=1)
-
     serializer = PermissionCreateSerializer(
         data={
-<<<<<<< feature_planning
             "permission_type": choice_value(PermissionType.WRITE),
             "start_date": timezone.now().date() + timedelta(days=1),
-=======
-            "permission_type": PermissionType.WRITE,
-            "start_date": future_date,
->>>>>>> develop
             "granted_to_user": normal_user.id,
         },
         context={"request": type("obj", (), {"user": admin_user})()},
